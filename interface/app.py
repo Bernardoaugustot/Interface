98187357fd43486ca6590880f9af37f98d670646
Lucas Amancio <74212108+lucasskt18@users.noreply.github.com>
2022-03-27 15:52:49 -0300

    Add files via upload

---
+++ interface/app.py
@@ -0,0 +1,25 @@
+from PySimpleGUI import PySimpleGUI as sg
+
+# Layout
+sg.theme('Reddit')
+layout = [
+   
+      [sg.Text('Usúario'), sg.Input(key = 'usuario')],
+      [sg.Text('Senha'), sg.Input(key = 'senha', password_char= '*')],
+      [sg.Checkbox('Salvar o login?'),],
+      [sg.Button('Entrar')],
+
+]
+
+# Janela
+janela = sg.Window('Tela de login', layout),
+# Ler os eventos da tela
+while True:
+   eventos, valores = janela.read()
+   if eventos == sg.WINDOW_CLOSED:
+         break
+   if eventos == 'Entrar':
+      if valores['usuario'] == 'lucas' and valores['senha']
+         == '123456':
+         print('Bem vindo!')
+
